399657efb72646f716d9e403b36c6a30c6022236
keyunzhang <104224930+keyunzhang@users.noreply.github.com>
2023-12-19 03:54:50 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ from .pascal_context import PascalContextDataset, PascalContextDataset59
 from .potsdam import PotsdamDataset
 from .stare import STAREDataset
 from .synapse import SynapseDataset
-from .pptDataset import pptDataset
+from .pptDataset import PptDataset
 # yapf: disable
 from .transforms import (CLAHE, AdjustGamma, BioMedical3DPad,
                          BioMedical3DRandomCrop, BioMedical3DRandomFlip,
@@ -49,5 +49,5 @@ __all__ = [
     'DecathlonDataset', 'LIPDataset', 'ResizeShortestEdge',
     'BioMedicalGaussianNoise', 'BioMedicalGaussianBlur',
     'BioMedicalRandomGamma', 'BioMedical3DPad', 'RandomRotFlip',
-    'SynapseDataset', 'pptDataset'
+    'SynapseDataset', 'PptDataset'
 ]
